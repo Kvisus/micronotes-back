d05e29c989b1455007f30b7de98c3e1893d068e9
Andrew Ilin <eternal.wanderer.z@gmail.com>
2025-12-16 22:11:59 +0300

    feat(user-service): migration, router

--- services/user-service/src/routes.ts
+++ services/user-service/src/routes.ts
@@ -1,5 +1,22 @@
 import { Router } from "express";
+import {
+  deleteUserProfile,
+  getUserProfile,
+  updateUserProfile,
+} from "./userController";
+import { authenticateToken, validateRequest } from "../../../shared/middleware";
+import { updateProfileSchema } from "./validation";
 
 const router = Router();
+
+// protected routes
+router.get("/profile", authenticateToken, getUserProfile);
+router.put(
+  "/profile",
+  authenticateToken,
+  validateRequest(updateProfileSchema),
+  updateUserProfile
+);
+router.delete("/profile", authenticateToken, deleteUserProfile);
 
 export default router;
